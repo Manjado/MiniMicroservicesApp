df15c554600db723b51739c3b38bebf9a4cc8398
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-07-09 23:02:26 +0200

    moderation service

--- query/index.js
+++ query/index.js
@@ -14,19 +14,32 @@ app.get("/posts", (req, res) => {
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
-  console.log("eventssss");
+
   if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
+
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    console.log(post, "POSTOTOTOTOTOTO");
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   console.log(posts);
   res.send({});
 });
